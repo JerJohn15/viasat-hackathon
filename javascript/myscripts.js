3e7da7717cc280a62ebf89606ab9fedef9797f4e
david dennis <david.dennis@viasat.com>
2016-07-16 11:56:53 -0600

    I HATE MY LIFE.

--- javascript/myscripts.js
+++ javascript/myscripts.js
@@ -3,7 +3,7 @@
  */
 
 function initMap() {
-    var mapDiv = document.getElementById('map');
+    var mapDiv = document.getElementById('map-canvas');
     var map = new google.maps.Map(mapDiv, {
         center: {lat: 44.540, lng: -78.546},
         zoom: 8
