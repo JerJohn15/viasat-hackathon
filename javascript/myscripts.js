d138a81c4fa09d9786d983509aa197741f9a9b98
david dennis <david.dennis@viasat.com>
2016-07-16 11:39:30 -0600

    i hate google maps

--- javascript/myscripts.js
+++ javascript/myscripts.js
@@ -1,3 +1,11 @@
 /**
  * Created by ddennis on 7/16/2016.
  */
+
+function initMap() {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+        center: {lat: 44.540, lng: -78.546},
+        zoom: 8
+    });
+}
